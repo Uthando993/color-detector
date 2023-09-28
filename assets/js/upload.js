e638f3d97eea89c62d1da4896c1a3380c5cf1a9f
Snehil Pandey <76873947+snehil-pandey@users.noreply.github.com>
2023-09-29 05:29:43 +0530

    Update upload.js

--- assets/js/upload.js
+++ assets/js/upload.js
@@ -1,8 +1,11 @@
 const file = document.querySelector('#file')
+const im = document.querySelector('img')
+
 file.addEventListener("change", function() {
-    const r = new FileReader()
-    r.addEventListener("load", () => {
-        document.querySelector("#image").src = r.result
-    })
-    r.readAsDataURL(this.files[0])
+  const r = new FileReader()
+  r.addEventListener("load", () => {
+    im.style.transform = 'scale('+1+')'
+    document.querySelector("#image").src = r.result
+  })
+  r.readAsDataURL(this.files[0])
 })
