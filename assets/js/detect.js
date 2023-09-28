9a7e901d5881a0d3c0008a04d030e7fd025d5576
Snehil Pandey <76873947+snehil-pandey@users.noreply.github.com>
2023-09-28 18:55:25 +0530

    Update detect.js

--- assets/js/detect.js
+++ assets/js/detect.js
@@ -68,3 +68,79 @@ function componentToHex(c) {
 function rgbToHex(r, g, b) {
   return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b)
 }
+
+//////////
+
+const picContainer = _('#pic')
+const imgElement = img
+let scale = 1
+let isPinching = false
+let initialDistance = 0
+let minScale = 1
+
+// Function to calculate the minimum scale to fit within the div frame
+function calculateMinScale() {
+  const containerWidth = picContainer.offsetWidth
+  const containerHeight = picContainer.offsetHeight
+  const imgWidth = imgElement.width
+  const imgHeight = imgElement.height
+
+  // Calculate the minimum scale to fit within either the width or height of the container
+  const widthScale = containerWidth / imgWidth
+  const heightScale = containerHeight / imgHeight
+  minScale = Math.max(widthScale, heightScale)
+}
+
+// Function to update the zoom level
+function updateZoom() {
+  // Ensure that the scale does not go below the minimum scale
+  scale = Math.max(minScale, scale)
+  imgElement.style.transformOrigin = 'top left' // Set the transform origin to the top-left corner
+  imgElement.style.transform = `scale(${scale})`
+}
+
+// Function to enable zooming for the img element
+function enableZoomForImg() {
+  imgElement.addEventListener('touchstart', (event) => {
+    if (event.touches.length === 2) {
+      isPinching = true
+      initialDistance = Math.hypot(
+        event.touches[0].clientX - event.touches[1].clientX,
+        event.touches[0].clientY - event.touches[1].clientY
+      )
+    }
+  })
+
+  imgElement.addEventListener('touchmove', (event) => {
+    if (isPinching && event.touches.length === 2) {
+      const currentDistance = Math.hypot(
+        event.touches[0].clientX - event.touches[1].clientX,
+        event.touches[0].clientY - event.touches[1].clientY
+      )
+      const delta = currentDistance - initialDistance
+      const zoomFactor = 0.01 // Adjust this value for sensitivity
+      scale += delta * zoomFactor
+      updateZoom()
+      initialDistance = currentDistance
+    }
+  })
+
+  imgElement.addEventListener('touchend', () => {
+    isPinching = false
+  })
+
+  // Initial zoom
+  calculateMinScale()
+  updateZoom()
+}
+
+// Detect if the user is on a mobile device
+function isMobileDevice() {
+  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
+}
+
+// Check if the device is mobile and enable zooming
+if (isMobileDevice()) {
+  // Enable zooming for the existing img element
+  enableZoomForImg()
+}
